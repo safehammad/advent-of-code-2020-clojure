05ea55698364bb6d22ce51ef6bc109a1b36837c8
Safe Hammad <safe.hammad@sandacre.com>
2020-12-05 06:20:04 +0000

    Add tests

--- test/safehammad/aoc2020_test.clj
+++ test/safehammad/aoc2020_test.clj
@@ -1,7 +1,16 @@
 (ns safehammad.aoc2020-test
   (:require [clojure.test :refer :all]
-            [safehammad.aoc2020 :refer :all]))
+            [safehammad.day1 :as day1]
+            [safehammad.day2 :as day2]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest day1-test
+  (testing "Day 1, part 1."
+    (is (= 181044 (day1/run 2))))
+  (testing "Day 1, part 2."
+    (is (= 82660352 (day1/run 3)))))
+
+(deftest day2-test
+  (testing "Day 2, part 1."
+    (is (= 447 (day2/run 1))))
+  (testing "Day 2, part 2."
+    (is (= 249 (day2/run 2)))))
